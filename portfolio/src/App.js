c842bbdebfca9e8b76b7133d79b45a88ce5a2a7f
Leah Rieger <leastuart92@gmail.com>
2021-10-18 13:45:49 -0700

    install bootstrap

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,11 @@
+// import components at top
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // a function that is usable as an component
 // jsx code 
 function App() {
-  return <div>Hello!</div>
+  return <div>Hello!</div>;
 }
 
+// export to use 'import' into other parts of the app
 export default App;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// renders the components
 ReactDOM.render(<App />, document.getElementById('root'));
 
